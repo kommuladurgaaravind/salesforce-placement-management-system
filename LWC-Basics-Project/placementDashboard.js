367f21a61c7a5d16299eb0a86e7b5457141e6c5c
kommuladurgaaravind <23pa1a0484@vishnu.edu.in>
2026-07-31 11:14:15 +0530

    Create placementDashboard.js with initial data

---
+++ LWC-Basics-Project/placementDashboard.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class PlacementDashboard extends LightningElement {
+
+    todayDate = '31 July 2026';
+
+    companies = 25;
+
+    jobs = 63;
+
+    applications = 5;
+
+}
